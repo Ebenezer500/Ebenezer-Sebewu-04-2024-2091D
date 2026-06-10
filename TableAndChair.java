ee5ec4fe90e05236ec1253cc33f44ee4f867afef
Ebenezer500 <ebenezersebewu78@gmail.com>
2026-06-10 12:47:02 +0000

    Add files via upload

---
+++ TableAndChair.java
@@ -0,0 +1,10 @@
+public class TableAndChair{
+    public static void main(String[] args) {
+        System.out.println("T                          T");
+        System.out.println("T                          T");
+        System.out.println("T       TTTTTTTTTTTTT      T");
+        System.out.println("TTTTT  T            T  TTTTT");
+        System.out.println("T   T  T            T  T   T");
+        System.out.println("T   T  T            T  T   T");
+    }
+}
